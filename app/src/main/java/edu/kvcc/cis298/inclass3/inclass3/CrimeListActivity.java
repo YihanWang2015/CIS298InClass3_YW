a89c037bbe55c920fe4cbc93daa38f7861ac65d6
Yihan Wang <ywang4241@mail.kvcc.edu>
2015-11-02 16:15:58 -0500

    Create SingleFragmentActivity.java as abstract superclass; Modify manifest file; Add CrimeListActivity & CrimeListFragment; Add fragment_crime_list.xml and add RecyclerView to it Set up the view for CrimeListFragment

---
+++ app/src/main/java/edu/kvcc/cis298/inclass3/inclass3/CrimeListActivity.java
@@ -0,0 +1,15 @@
+package edu.kvcc.cis298.inclass3.inclass3;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ywang4241 on 11/2/2015.
+ */
+public class CrimeListActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
